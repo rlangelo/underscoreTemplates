b0bad3972ba4e426b8f7d3918dd3450ce8309d5b
Rafael Angelo <rlangelo@wpi.edu>
2015-12-11 17:33:50 -0500

    Almost finalized. Needs 1 layout fix

--- public/script.js
+++ public/script.js
@@ -92,17 +92,20 @@ function generateCharacter() {
 	var complete = checkFormComplete();
 	if (complete == 1) {
 		assignHeadAttr();
+		assignTorsoAttr();
+		assignLegsAttr();
 		if (document.getElementById("male").checked) {
 			gender = "Male";
-			characters.push("{name: " + name + ", \ngender: " + gender + ", \nhattr1: " + hattr1 + ", \nhattr2: " + hattr2 +
-												", \nhattr3: " + hattr3 + " },\n");
 		}
 		else {
 			gender = "Female";
-			characters.push("{name: " + name + ", \ngender: " + gender + ", \nhattr1: " + hattr1 + ", \nhattr2: " + hattr2 +
-												", \nhattr3: " + hattr3 + " },\n");
 		}
+		characters.push("{name: " + name + ", \ngender: " + gender + ", \nhattr1: " + hattr1 + ", \nhattr2: " + hattr2 +
+											", \nhattr3: " + hattr3 + ", \ntattr1: " + tattr1 + ", \ntattr2: " + tattr2 +
+											", \ntattr3: " + tattr3 + ", \ntattr4: " + tattr4 + ", \nlattr1: " + lattr1 +
+											", \nlattr2: " + lattr2 + " },\n");
 		refreshList();
+		startOver();
 	}
 	else {
 		alert("Please fill all fields before generating")
@@ -110,7 +113,6 @@ function generateCharacter() {
 }
 
 function assignHeadAttr() {
-	var val;
 	var radios1 = document.getElementById("head1").getElementsByTagName('input');
 	for (var i=0; i<radios1.length; i++) {
 		if (radios1[i].type == 'radio') {
@@ -132,6 +134,61 @@ function assignHeadAttr() {
 		if (radios3[i].type == 'radio') {
 			if (radios3[i].checked) {
 				hattr3 = radios3[i].value;
+			}
+		}
+	}
+}
+
+function assignTorsoAttr() {
+	var radios1 = document.getElementById("torso1").getElementsByTagName('input');
+	for (var i=0; i<radios1.length; i++) {
+		if (radios1[i].type == 'radio') {
+			if (radios1[i].checked) {
+				tattr1 = radios1[i].value;
+			}
+		}
+	}
+	var radios2 = document.getElementById("torso2").getElementsByTagName('input');
+	for (var i=0; i<radios2.length; i++) {
+		if (radios2[i].type == 'radio') {
+			if (radios2[i].checked) {
+				tattr2 = radios2[i].value;
+			}
+		}
+	}
+	var radios3 = document.getElementById("torso3").getElementsByTagName('input');
+	for (var i=0; i<radios3.length; i++) {
+		if (radios3[i].type == 'radio') {
+			if (radios3[i].checked) {
+				tattr3 = radios3[i].value;
+			}
+		}
+	}
+
+	var radios4 = document.getElementById("torso4").getElementsByTagName('input');
+	for (var i=0; i<radios3.length; i++) {
+		if (radios3[i].type == 'radio') {
+			if (radios3[i].checked) {
+				tattr4 = radios4[i].value;
+			}
+		}
+	}
+}
+
+function assignLegsAttr() {
+	var radios1 = document.getElementById("legs1").getElementsByTagName('input');
+	for (var i=0; i<radios1.length; i++) {
+		if (radios1[i].type == 'radio') {
+			if (radios1[i].checked) {
+				lattr1 = radios1[i].value;
+			}
+		}
+	}
+	var radios2 = document.getElementById("legs2").getElementsByTagName('input');
+	for (var i=0; i<radios2.length; i++) {
+		if (radios2[i].type == 'radio') {
+			if (radios2[i].checked) {
+				lattr2 = radios2[i].value;
 			}
 		}
 	}
@@ -203,7 +260,7 @@ function colorChange3() {
 }
 
 var compiled = _.template(
-	"<div class='post'>" +
+	"<div class='namePost'>" +
 	  "<a><%= name %></a>" +
 		"<a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;&nbsp&nbsp</a>" +
 	  "<a><%= gender %></a>" +
@@ -227,21 +284,26 @@ var torsoCompiled = _.template(
 );
 
 var legsCompiled = _.template(
-	"<div class='post'>" +
-	"<a><%= lattr1 %></a>" +
-	"<a><%= lattr2 %></a>" +
+	"<div class='legPost'>" +
+	"<p><a><%= lattr1 %></a><a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp&nbsp</a><a><%= lattr2 %></a></p>" +
 	"</div>"
 );
 
 var str = "";
 var strHead = "";
+var strTorso = "";
+var strLegs = "";
 
 function refreshList() {
 	characters.forEach( function(p, i) {
 		str = compiled(p);
 		strHead = headCompiled(p);
+		strTorso = torsoCompiled(p);
+		strLegs = legsCompiled(p);
 	})
 
 	document.getElementById("list").innerHTML += str;
 	document.getElementById("list").innerHTML += strHead;
+	document.getElementById("list").innerHTML += strTorso;
+	document.getElementById("list").innerHTML += strLegs;
 }
